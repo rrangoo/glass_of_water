843d8889fed610dc4debbff9f3024204dc0780f1
danilk3 <dmkotelnikov@edu.hse.ru>
2023-03-24 00:02:17 +0300

    fix getNotFriends method v3

--- src/main/java/ru/hse/glassofwater/service/FriendsService.java
+++ src/main/java/ru/hse/glassofwater/service/FriendsService.java
@@ -84,6 +84,6 @@ public class FriendsService {
     public List<User> getNotFriends(Long userId) {
         User currentUser = userRepo.findById(userId).get();
         List<User> allUsers = userRepo.findAll();
-        return allUsers.stream().filter(user -> !currentUser.getFriends().contains(user)).collect(Collectors.toList());
+        return allUsers.stream().filter(user -> !currentUser.getFriends().contains(user) && !user.equals(currentUser)).collect(Collectors.toList());
     }
 }
